51e0d1f7cd18599d77d59bfd026b10d2abe1a46d
Rishbh Rana <ranaris@kean.edu>
2017-04-20 10:14:02 -0400

    Delete config.php

--- ToBeSorted/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-   define('DB_SERVER', 'imc.eve.kean.edu');
-   define('DB_USERNAME', 'ranaris');
-   define('DB_PASSWORD', '0843564');
-   define('DB_DATABASE', 'CPS3740_2016S.Users');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE) or die('Could not connect to the server: '.mysqli_connect_error() );
-   
-?>
