45337fe40077074d20d534e63a7d258ed3163a1e
rishbhr <ranaris@kean.edu>
2017-04-20 17:48:31 -0400

    clean up

--- ToBeSorted/quiz1.php
+++
@@ -1,6 +0,0 @@
-<?php
-	//Display welcome message on webpage
-	echo"Welcome to CPS 3740-03<br> <strong>";
-	echo"Rana, Rishbh<br> <strong>";
-	echo"Computer Science, Kean University<br> <strong>";	
-?>
